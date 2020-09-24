67c6ce167a0d248937dd54bfe22101b510fa7fac
Srinivasu.Gannem <srinivasu.gannem@nxp.com>
2020-09-24 22:26:28 +0530

    feat(test1):test1

---
+++ src/main/java/de/gliderpilot/semanticrelease/test/Test.java
@@ -0,0 +1,13 @@
+package de.gliderpilot.semanticrelease.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(greet("Hello", "World"));
+    }
+
+    public static String greet(String greeting, String who) {
+        return greeting + " " + who;
+    }
+
+}
